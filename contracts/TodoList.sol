781c652ab8ce50c02c601027252bc55c932fd9e1
themsiqueira <mateussiqueiracarneiro@outlook.com>
2019-10-11 11:23:26 -0300

    first commit

---
+++ contracts/TodoList.sol
@@ -0,0 +1,8 @@
+//its necessary to expicified the language version i use
+pragma solidity ^0.5.0;
+
+//now we declare the smart contract
+contract TodoList {
+  //state variable, represent state of this smart contract on a blockchain
+  uint public taskCount = 0;
+}
